e1490fae3f4d3d802c86ba729895f7300b4c7a3a
oscar_mesa <oscarmesa.elpoli@gmail.com>
2020-08-21 13:02:24 -0500

    cambios

--- src/components/seccion2/DetalleIMCApp.jsx
+++ src/components/seccion2/DetalleIMCApp.jsx
@@ -6,15 +6,24 @@ export default class DetalleIMCApp extends Component{
     constructor(props){
         console.log(props);
         super(props);
-        this.nombre = "oscar mesa";
+        this.state = {
+            numero : 10,
+            edad : props.edad
+        }
+        this.sumar = this.sumar.bind(this);
+    }
+
+    sumar(e){
+        this.setState({numero : this.state.numero + 1})
     }
 
     render(){
+        console.log("Entro y numero vale "+this.state.numero)
         return (<section>
                     <h1>Detalle</h1>
-                    <button onClick={function(){
-                        alert("Hola")
-                    }}>Saludar!!!</button>
+                    <button onClick={this.sumar}>Sumar</button>
+                    <input type="text"/>
+                    <p>Numero = {this.state.numero}</p>
                 </section>
         );
     }
